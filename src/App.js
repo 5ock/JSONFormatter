423e01c80793db55e1b44f52875f07728370b04f
5ock <ccrubio5@gmail.com>
2021-07-28 14:26:00 +0800

    create json formatter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useEffect } from 'react'
+import PropTypes from 'prop-types'
+import Button from '@material-ui/core/Button'
+import Grid from '@material-ui/core/Grid'
+import { makeStyles } from '@material-ui/core/styles'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const useStyles = makeStyles((theme) => ({
+	root: {
+		marginTop: '30px',
+		flexGrow: 1,
+	},
+	block: {
+		textAlign: 'center'
+	},
+	textareaStyle: {
+		width: '80%',
+		resize: 'vertical'
+	},
+}))
+
+const App = (props) => {
+	const { id } = props
+	const jsonData = useRef()
+	const classes = useStyles()
+	const [ jsonFormatted, setJsonFormatted ] = useState()
+
+	const handleFormat = () => {
+		if(typeof JSON.parse(jsonData.current) === 'object')
+		setJsonFormatted(JSON.stringify(JSON.parse(jsonData.current), null, 4))
+	}
+
+	return (<div id={id}>
+		<Grid container className={classes.root}>
+        	<Grid item xs={5} className={classes.block}>
+				<textarea className={classes.textareaStyle} onChange={ v => jsonData.current = v.target.value } rows='40' />
+			</Grid>
+        	<Grid item xs={2} className={classes.block}>
+				<Button variant="contained" color="primary" onClick={handleFormat}>Process</Button>
+			</Grid>	
+        	<Grid item xs={5} className={classes.block}>
+				<textarea className={classes.textareaStyle} rows='40' value={jsonFormatted} />
+			</Grid>
+		</Grid>
+	</div>)
+}
+
+App.propTypes = {
+	id: PropTypes.string
+}
+
+App.defaultProps = {
+	id: 'g-json-format'
 }
 
 export default App;
