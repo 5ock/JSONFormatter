e874ff9258c428074e61d2d1e81bb9c62c6d6def
5ock <ccrubio5@gmail.com>
2022-01-07 11:22:09 +0800

    Update UI

--- src/App.js
+++ src/App.js
@@ -7,11 +7,12 @@ import './App.css'
 
 const useStyles = makeStyles((theme) => ({
 	root: {
-		marginTop: '30px',
+		marginTop: '20px',
 		flexGrow: 1,
 	},
 	block: {
-		textAlign: 'center'
+		textAlign: 'center',
+		marginBottom: '10px'
 	},
 	textareaStyle: {
 		width: '80%',
@@ -32,13 +33,13 @@ const App = (props) => {
 
 	return (<div id={id}>
 		<Grid container className={classes.root}>
-        	<Grid item xs={5} className={classes.block}>
-				<textarea className={classes.textareaStyle} onChange={ v => jsonData.current = v.target.value } rows='40' />
+        	<Grid item xs={12} className={classes.block}>
+				<textarea className={classes.textareaStyle} onChange={ v => jsonData.current = v.target.value } rows='5' />
 			</Grid>
-        	<Grid item xs={2} className={classes.block}>
+        	<Grid item xs={12} className={classes.block}>
 				<Button variant="contained" color="primary" onClick={handleFormat}>Process</Button>
 			</Grid>	
-        	<Grid item xs={5} className={classes.block}>
+        	<Grid item xs={12} className={classes.block}>
 				<textarea className={classes.textareaStyle} rows='40' value={jsonFormatted} />
 			</Grid>
 		</Grid>
